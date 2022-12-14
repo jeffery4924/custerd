5a4c09796d58ed28845a8cd56ea69988573f147e
jeffery4924 <96092255+jeffery4924@users.noreply.github.com>
2022-12-13 21:59:53 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(1.tostr());
+
+
+
+
+public static class StringExtensions
+{
+    public static string tostr(this int s)
+    {
+        return "\"" + (s.ToString()) + "\"";
+    }
+}
